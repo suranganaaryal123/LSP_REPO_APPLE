3feea6589298ed5ee6ca7189e4e10339890c41b7
Surangana Aryal <145215217+suranganaaryal123@users.noreply.github.com>
2024-10-17 20:22:55 -0400

    Update Calculator.java

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculator.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question2/Calculator.java
@@ -1,10 +1,24 @@
 package org.howard.edu.lsp.midterm.question2;
 
-public class HelloWorld {
+public class Calculator {
+	//adds two integers together and returns the result
+    public static int sum(int a, int b) {
+        return a + b;
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
-		System.out.println("Hello World!!!");
-	}
+    // Sum two double values
+    public static double sum(double a, double b) {
+        return a + b;
+    }
 
+    // Sum array of integers
+    public static int sum(int[] arr) {
+        int total = 0;
+        // loops through ecah number
+        for (int num : arr) {
+            total += num;
+        }
+        return total;
+    }
 }
+
