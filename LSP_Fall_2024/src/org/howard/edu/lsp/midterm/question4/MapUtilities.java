21e89226f8a0d1b4c9c6d217524836cd09bca631
Surangana Aryal <145215217+suranganaaryal123@users.noreply.github.com>
2024-10-17 20:25:05 -0400

    Update MapUtilities.java

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question4/MapUtilities.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question4/MapUtilities.java
@@ -1,10 +1,25 @@
 package org.howard.edu.lsp.midterm.question4;
 
-public class HelloWorld {
+import java.util.HashMap;
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+public class MapUtilities {
+	//counting how many key/value pairs are the same in both maps
+    public static int commonKeyValuePairs(HashMap<String, String> map1, HashMap<String, String> map2) {
+        int count = 0;
 
-	}
+        // Check if either map is empty
+        if (map1.isEmpty() || map2.isEmpty()) {
+            return 0;
+        }
 
+        // Loop through each key in map1
+        for (String key : map1.keySet()) {
+        	// Check if map2 has the same key and if the values match
+            if (map2.containsKey(key) && map1.get(key).equals(map2.get(key))) {
+                count++;
+            }
+        }
+        return count;//total number of common pairs we found
+    }
 }
+
