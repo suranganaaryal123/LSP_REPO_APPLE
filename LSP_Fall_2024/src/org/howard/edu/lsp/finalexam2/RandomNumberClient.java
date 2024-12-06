fffcadd8d9b29a852dd52cdf1b51835b22f19ed6
Surangana Aryal <145215217+suranganaaryal123@users.noreply.github.com>
2024-12-05 21:03:48 -0500

    Update RandomNumberClient.java

--- LSP_Fall_2024/src/org/howard/edu/lsp/finalexam2/RandomNumberClient.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam2/RandomNumberClient.java
@@ -3,14 +3,14 @@ package src.org.howard.edu.lsp.finalexam2;
 public class RandomNumberClient {
 
     public static void main(String[] args) {
-        // Get the singleton instance of the RandomNumberService
+        // Get the singleton instance 
         RandomNumberService service = RandomNumberService.getInstance();
 
-        // Using the built-in random number generator strategy
+        // built-in random number generator strategy
         service.setStrategy(new BuiltInRandomGenerator());
         System.out.println("Random number using BuiltInRandomGenerator: " + service.generateRandomNumber());
 
-        // Using the custom random number generator strategy with a seed of 12345
+        // custom random number generator strategy 
         service.setStrategy(new CustomRandomGenerator(12345));
         System.out.println("Random number using CustomRandomGenerator: " + service.generateRandomNumber());
     }
