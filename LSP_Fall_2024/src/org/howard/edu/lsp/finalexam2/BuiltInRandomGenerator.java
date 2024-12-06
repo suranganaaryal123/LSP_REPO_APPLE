f1a3ea92c328ed2422537c17d86e65f6ca918665
Surangana Aryal <145215217+suranganaaryal123@users.noreply.github.com>
2024-12-05 21:02:52 -0500

    Update BuiltInRandomGenerator.java

--- LSP_Fall_2024/src/org/howard/edu/lsp/finalexam2/BuiltInRandomGenerator.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam2/BuiltInRandomGenerator.java
@@ -3,18 +3,18 @@ package src.org.howard.edu.lsp.finalexam2;
 import java.util.Random;
 
 /**
- * Random number generator using Java's built-in Random class. abcd check
+ * Random number generator abcd check
  */
 public class BuiltInRandomGenerator implements RandomNumberGenerator {
     private final Random random = new Random();
 
     /**
-     * Generates a random number between 1 and Integer.MAX_VALUE.
+     * Generates a random number 
      * 
      * @return a random positive integer.
      */
     @Override
     public int generate() {
-        return random.nextInt(Integer.MAX_VALUE) + 1; // Ensures positive integers
+        return random.nextInt(Integer.MAX_VALUE) + 1; 
     }
 }
